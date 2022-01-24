1c1693fb19010d26b82bffe3b988bf22944a6751
Aviva Shuman <97950104+Ashugirl@users.noreply.github.com>
2022-01-24 12:09:27 +0100

    Add files via upload

---
+++ App.java
@@ -0,0 +1,24 @@
+package com.javastudent;
+
+public class App {
+	
+	public static void main(String[] args) {
+		// Math Object
+		int a=12; int b=11;
+		// Max Method Find Highest number!
+		System.out.println(Math.max(a,b));
+		System.out.println(Math.PI);
+		System.out.println(Math.min(a,b));
+		System.out.println(Math.sqrt(32));
+		System.out.println(Math.abs(-10));
+		double getNumber = Math.random() * 10;
+		System.out.println(Math.round(getNumber));
+		String[] names = {"Ali","Veli","Deli"};
+		double GenIndexNum = Math.random() *2;
+		System.out.println(names[0]);
+		System.out.println(names[(int) Math.round(GenIndexNum)]);
+		
+		
+	}
+
+}
